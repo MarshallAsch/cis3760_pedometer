5915866608438a8e49faecd52d9bb0451ca32890
Marshall Asch <maasch@rogers.com>
2018-03-21 12:59:58 -0400

    idk what this is, some change that android wanted

--- src/main/java/de/j4velin/pedometer/ui/Fragment_Settings.java
+++ src/main/java/de/j4velin/pedometer/ui/Fragment_Settings.java
@@ -33,7 +33,6 @@ import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceFragment;
-import android.preference.SwitchPreference;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
