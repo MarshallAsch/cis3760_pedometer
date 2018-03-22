8afdb6519ccb918cd8b5cb1cf79d0b55704522c5
weihan <weihanli101@gmail.com>
2018-03-22 01:16:53 -0400

    added graph functionality

--- src/main/java/de/j4velin/pedometer/ui/Fragment_Analytics.java
+++ src/main/java/de/j4velin/pedometer/ui/Fragment_Analytics.java
@@ -5,12 +5,14 @@ import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -59,16 +61,6 @@ public class Fragment_Analytics extends android.app.Fragment {
         distanceView = (TextView) v.findViewById(R.id.distance);
         caloriesView = (TextView) v.findViewById(R.id.calories);
 
-        //sample graphing points
-        //TODO:: replace with real values
-        GraphView graph = (GraphView) v.findViewById(R.id.graph);
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
-                new DataPoint(0, 1),
-                new DataPoint(1, 5),
-                new DataPoint(2, 3)
-        });
-        graph.addSeries(series);
-
         //showing month selections for spinner drop down
         Spinner timeSpinner = (Spinner) v.findViewById(R.id.time_label);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this.getActivity(),
@@ -76,10 +68,144 @@ public class Fragment_Analytics extends android.app.Fragment {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         timeSpinner.setAdapter(adapter);
 
+        //Default place holder value so the graph appears even if there is no data
+        GraphView graph = (GraphView) v.findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, 0),
+        });
+        graph.addSeries(series);
+
+        timeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if(adapterView.getItemAtPosition(i).toString().equals("All Time")){
+                    Log.d("Analytics","All Time");
+                    //TODO: change fake values
+                    GraphView graph = (GraphView) v.findViewById(R.id.graph);
+                    graph.removeAllSeries();
+                    LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+                            new DataPoint(0, 1400204),
+                            new DataPoint(1, 1205125),
+                            new DataPoint(2, 1099323),
+                            new DataPoint(3, 1600633),
+                            new DataPoint(4, 1200432),
+                            new DataPoint(5, 1350295),
+                            new DataPoint(6, 1400595),
+                            new DataPoint(7, 1220001),
+                    });
+                    graph.addSeries(series);
+
+                }
+                else if(adapterView.getItemAtPosition(i).toString().equals("Year")){
+                    Log.d("Analytics","Year");
+                    //TODO: change fake values
+                    GraphView graph = (GraphView) v.findViewById(R.id.graph);
+                    graph.removeAllSeries();
+                    LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+                            new DataPoint(1, 40200),
+                            new DataPoint(2, 10200),
+                            new DataPoint(3, 11142),
+                            new DataPoint(4, 14101),
+                            new DataPoint(5, 11542),
+                            new DataPoint(6, 12041),
+                            new DataPoint(7, 19004),
+                            new DataPoint(8, 61111),
+                            new DataPoint(9, 44122),
+                            new DataPoint(10, 10221),
+                            new DataPoint(11, 11901),
+                            new DataPoint(12, 14018),
+                    });
+                    graph.addSeries(series);
+
+
+                }
+                else if(adapterView.getItemAtPosition(i).toString().equals("Month")){
+                    //TODO: change fake values
+                    Log.d("Analytics","Month");
+                    GraphView graph = (GraphView) v.findViewById(R.id.graph);
+                    graph.removeAllSeries();
+                    LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+                            new DataPoint(1, 4022),
+                            new DataPoint(2, 1020),
+                            new DataPoint(3, 1190),
+                            new DataPoint(4, 1401),
+                            new DataPoint(5, 1150),
+                            new DataPoint(6, 1204),
+                            new DataPoint(7, 1900),
+                            new DataPoint(8, 6002),
+                            new DataPoint(9, 4022),
+                            new DataPoint(10, 1020),
+                            new DataPoint(11, 1190),
+                            new DataPoint(12, 1401),
+                            new DataPoint(13, 1150),
+                            new DataPoint(14, 1204),
+                            new DataPoint(15, 1900),
+                            new DataPoint(16, 6008),
+                            new DataPoint(17, 4029),
+                            new DataPoint(18, 1020),
+                            new DataPoint(19, 1190),
+                            new DataPoint(20, 1401),
+                            new DataPoint(21, 1150),
+                            new DataPoint(22, 1204),
+                            new DataPoint(23, 1900),
+                            new DataPoint(24, 6001),
+                            new DataPoint(25, 4022),
+                            new DataPoint(26, 1020),
+                            new DataPoint(27, 1190),
+                            new DataPoint(28, 1401),
+                            new DataPoint(29, 1150),
+                            new DataPoint(30, 1204),
+                            new DataPoint(31, 1900),
+                    });
+                    graph.addSeries(series);
+
+                }
+                else if(adapterView.getItemAtPosition(i).toString().equals("Day")){
+                    Log.d("Analytics","Day");
+                    //TODO: change fake values
+                    GraphView graph = (GraphView) v.findViewById(R.id.graph);
+                    graph.removeAllSeries();
+                    LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+                            new DataPoint(0, 0),
+                            new DataPoint(1, 0),
+                            new DataPoint(2, 0),
+                            new DataPoint(3, 0),
+                            new DataPoint(4, 0),
+                            new DataPoint(5, 0),
+                            new DataPoint(6, 0),
+                            new DataPoint(7, 0),
+                            new DataPoint(8, 0),
+                            new DataPoint(9, 0),
+                            new DataPoint(10, 150),
+                            new DataPoint(11, 30),
+                            new DataPoint(12, 40),
+                            new DataPoint(13, 50),
+                            new DataPoint(14, 80),
+                            new DataPoint(15, 90),
+                            new DataPoint(16,240),
+                            new DataPoint(17, 144),
+                            new DataPoint(18, 14),
+                            new DataPoint(19, 5),
+                            new DataPoint(20, 3),
+                            new DataPoint(21, 145),
+                            new DataPoint(22, 10),
+                            new DataPoint(23, 15),
+                            new DataPoint(24, 1),
+                    });
+                    graph.addSeries(series);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
 
         //Get Db instance to use db functions
         Database db = Database.getInstance(getActivity());
-        total_start = db.getTotalWithoutToday();
+        total_start = db.getAllSteps();
         total_days = db.getDays();
 
 
